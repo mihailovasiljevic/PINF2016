d0dad05574a1f926c9a0e578564f14fdca91af81
Nemanja <Nemanja@DESKTOP-O4OEVJU>
2016-05-05 00:54:32 +0200

    Isti kao prethodni commit.

---
+++ src/database/ColumnDescription.java
@@ -0,0 +1,64 @@
+package database;
+
+public class ColumnDescription {
+
+	private String code;
+	private String label;
+	private String type;
+	private int length; 
+	private boolean primary_key;
+	private boolean foreign_key;
+	private boolean nullable;
+	private String tableParent;
+	
+	
+	public String getCode() {
+		return code;
+	}
+	public void setCode(String code) {
+		this.code = code;
+	}
+	public String getLabel() {
+		return label;
+	}
+	public void setLabel(String label) {
+		this.label = label;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public int getLength() {
+		return length;
+	}
+	public void setLength(int length) {
+		this.length = length;
+	}
+	public boolean isPrimary_key() {
+		return primary_key;
+	}
+	public void setPrimary_key(boolean primary_key) {
+		this.primary_key = primary_key;
+	}
+	public boolean isForeign_key() {
+		return foreign_key;
+	}
+	public void setForeign_key(boolean foreign_key) {
+		this.foreign_key = foreign_key;
+	}
+	public boolean isNullable() {
+		return nullable;
+	}
+	public void setNullable(boolean nullable) {
+		this.nullable = nullable;
+	}
+	public String getTableParent() {
+		return tableParent;
+	}
+	public void setTableParent(String tableParent) {
+		this.tableParent = tableParent;
+	}
+	
+}
