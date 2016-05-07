7df0089dc995aab21b053662e99a45e03be63fe3
Mihailo <mihailo.vasiljevic93@gmail.com>
2016-05-07 21:19:51 +0200

    JavaDoc commented IDataGetter.

--- src/database/crud/IQueryCreator.java
+++ src/database/crud/IQueryCreator.java
@@ -1,5 +1,13 @@
 package database.crud;
 
-public interface IQueryCreator {
+import java.sql.SQLException;
+import java.util.HashMap;
 
+public interface IQueryCreator {
+	int read(String tableName, String whereStmt, String orderBy) throws SQLException;
+	int create(HashMap<String, String> data) throws SQLException;
+	int update(HashMap<String, String> data, int index) throws SQLException;
+	int delete(int index) throws SQLException;
+	
+	void checkRow(int index) throws SQLException;
 }
