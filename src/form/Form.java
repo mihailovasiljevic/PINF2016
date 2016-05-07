2527b15067857269da54838b2f6d6a9190868649
MilosSavic <savic.milan@sezampro.rs>
2016-05-07 23:19:41 +0200

    Status bar

--- src/form/Form.java
+++ src/form/Form.java
@@ -16,6 +16,8 @@ public class Form extends JDialog {
 	public static TableDescription description;
 	private DataPanel dataPanel;
 	private MyTable table;
+	private StatusBar statusBar;
+	
 	
 	public Form(Window parent, TableDescription tdescription) {
 		super(parent,tdescription.getLabel());
@@ -24,8 +26,10 @@ public class Form extends JDialog {
 		this.setModal(true);
 		this.description = tdescription;
 		this.dataPanel=new DataPanel(description);
+		this.statusBar = new StatusBar();
 		this.init(tdescription);
 		this.setLocationRelativeTo(parent);
+		
 	}
 	
 	public Form(){
@@ -47,6 +51,8 @@ public class Form extends JDialog {
 		bottomPanel.add(new ButtonsPanel(this),"dock east");
 		
 		add(bottomPanel, "grow, wrap");
+		add(statusBar, "dock south");
+		statusBar.getStatLab1().setText(description.getCode());
 		/*Vector vect = new Vector();
 		vect.add("A"); vect.add("B");
 		table.addInTable(vect);*/
