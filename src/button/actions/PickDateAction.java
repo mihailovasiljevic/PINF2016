0ddec2b28584794f2f683c3270bfc192247666f6
Raludza <rale.ilic1414@gmail.com>
2016-07-13 19:20:12 +0200

    DatePicker for report

--- src/button/actions/PickDateAction.java
+++ src/button/actions/PickDateAction.java
@@ -5,8 +5,10 @@ import java.awt.event.ActionListener;
 
 import javax.swing.JTextField;
 
+import form.DatumForm;
 import form.Form;
 import form.DatePicker;
+
 
 
 import javax.swing.JTextField;
@@ -15,17 +17,28 @@ public class PickDateAction implements ActionListener{
 
 	private JTextField textField;
 	private Form form;
+	private DatumForm dateform =new DatumForm();
 
 	public PickDateAction(Form form, JTextField textField) {
 		this.setTextField(textField);
 		this.setForm(form);
 	}
-	
+
+	public PickDateAction(JTextField textField) {
+		// TODO Auto-generated constructor stub
+		this.setTextField(textField);
+	}
+
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		DatePicker dpck = new DatePicker(form);
-		textField.setText(dpck.getPickedDate());
-		
+		if(form!=null){
+			DatePicker dpck = new DatePicker(form);
+			textField.setText(dpck.getPickedDate());
+		}else{
+			DatePicker dpck = new DatePicker(dateform);
+			textField.setText(dpck.getPickedDate());
+		}
+
 	}
 
 	public JTextField getTextField() {
