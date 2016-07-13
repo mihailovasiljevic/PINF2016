7a9e1e6963c374e8f4f180e2cf149e916f248d9f
remixgit <mihailo.vasiljevic93@gmail.com>
2016-07-13 19:13:27 +0200

    Working on radial button group test.

--- src/form/DataPanel.java
+++ src/form/DataPanel.java
@@ -3,10 +3,12 @@ package form;
 import java.awt.Component;
 import java.awt.Dimension;
 import java.text.Format;
+import java.util.Enumeration;
 import java.util.Formatter;
 import java.util.Properties;
 import java.util.Vector;
 
+import javax.swing.AbstractButton;
 import javax.swing.ButtonGroup;
 import javax.swing.JButton;
 import javax.swing.JLabel;
@@ -92,6 +94,22 @@ public class DataPanel extends JPanel {
 					btnGroup.add(rBtnTrue);
 					btnGroup.add(rBtnFalse);
 					btnGroups.add(btnGroup);
+					
+					for(JSONModel model : MainFrame.getInstance().getJsonModels()){
+						if(model.getTableName().equalsIgnoreCase(form.getDescription().getCode())){
+							for(String field : model.getDisabledFields()){
+								if(field.equals(columnDescription.get(i).getCode())){
+									System.out.println("Disable button grupe.");
+									rBtnFalse.setEnabled(false);
+									rBtnTrue.setEnabled(false);
+									break;
+
+								}
+							}
+							break;
+						}
+					}
+					
 					this.add(rBtnTrue);
 					this.add(rBtnFalse, "wrap");
 
