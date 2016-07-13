a226f347b03e3376591dee775bc3b13e72ddd114
Nemanja <nemanja.s.993@hotmail.com>
2016-07-13 20:32:41 +0200

    popravio refresh

--- src/toolbar/actions/RefreshAction.java
+++ src/toolbar/actions/RefreshAction.java
@@ -9,7 +9,11 @@ import javax.swing.AbstractAction;
 import javax.swing.ImageIcon;
 import javax.swing.JDialog;
 
+import main.MainFrame;
 import main.MyMenuBar;
+import states.InsertState;
+import states.SearchState;
+import states.State;
 import table.MyTable;
 import table.MyTableModel;
 
@@ -28,15 +32,24 @@ public class RefreshAction extends AbstractAction {
 		
 		
 		if(standardForm instanceof Form){
-			System.out.print("ref");
+			State state = MainFrame.getInstance().getContext().getState();
+			int selected = ((Form) standardForm).getTable().getSelectedRow();
+			if(selected < 0) selected = 0;
 			try {
-				((Form) standardForm).refresh(0);
+				((Form) standardForm).refresh(selected);
 			} catch (Exception e) {
 				System.out.println("Tabela je najverovatnije prazna a pokusavate da uradite refresh.");
 			}
-			
+			if(state instanceof InsertState) {
+				MainFrame.getInstance().getContext().setState(state);
+				((Form) standardForm).getToolbarr().getAddAction().actionPerformed(null);
+			}
+			if(state instanceof SearchState) {
+				MainFrame.getInstance().getContext().setState(state);
+				((Form) standardForm).getToolbarr().getSearchAction().actionPerformed(null);
+			}			
 		}
-					
+			
 				
 			
 	}
