7d9ffd853f20e477c6eafee6486d224654ec817e
Nemanja Starčev <nemanja.s.993@hotmail.com>
2016-07-14 00:23:56 +0200

    Ponovo radi otvaranje poslovne godine

--- src/toolbar/actions/OpenYear.java
+++ src/toolbar/actions/OpenYear.java
@@ -7,7 +7,10 @@ import java.sql.SQLException;
 import java.util.Vector;
 
 import javax.swing.AbstractAction;
+import javax.swing.JOptionPane;
 
+import states.InsertState;
+import main.MainFrame;
 import database.DataBase;
 import form.Form;
 
@@ -29,9 +32,14 @@ public class OpenYear extends AbstractAction {
 		String POSL_GOD_ID = form.getDataPanel().getField("POSL_GOD_ID").getText();
 		
 		System.out.println("poslovna " + POSL_GOD_ID + " " + POSL_GOD_DAT_ID + " " + POSL_SIS_ID + " " + POSL_GOD_GOD );
-		
+		if(MainFrame.getInstance().getContext().getState() instanceof InsertState) {
+			
+		} else {
+			JOptionPane.showMessageDialog(null, "Nije moguce otvaranje poslovne godine ", "GRESKA", JOptionPane.ERROR_MESSAGE);
+			return;
+		}
 		try {
-			CallableStatement proc = DataBase.getConnection().prepareCall("{ call otvaranje_poslovne_godine(?,?,?)}");
+			CallableStatement proc = DataBase.getConnection().prepareCall("{ call otvaranje_poslovne_godine(?,?,?,?)}");
 				proc.setInt(1, Integer.parseInt(POSL_SIS_ID));
 				proc.setString(2, POSL_GOD_GOD);
 				
@@ -43,18 +51,22 @@ public class OpenYear extends AbstractAction {
 				Date date = new Date(Integer.parseInt(year)-1900, Integer.parseInt(month)-1, Integer.parseInt(day));
 				
 				proc.setDate(3, date);
+				proc.registerOutParameter(4, java.sql.Types.INTEGER);
 				proc.execute();
+				Integer id = proc.getInt(4);
+				Vector adding = new Vector();
+				adding.add(id.toString());
+				adding.add(POSL_SIS_ID);
+				adding.add(POSL_GOD_GOD);
+				adding.add(POSL_GOD_DAT_ID);
+				adding.add("");
+				adding.add("0");
+				form.getTable().getModel().addRow(adding);
 				DataBase.getConnection().commit();
-
 			} catch (SQLException ee) {
 				ee.printStackTrace();
 				return;
 			}
-		
-		Vector adding = new Vector();
-		
-		//form.getTable().getModel().addRow(rowData);
-		
 	}
 
 }
